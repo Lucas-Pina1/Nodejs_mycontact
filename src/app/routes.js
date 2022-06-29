40e2bcb3bf08d10c842ea67db2ed2c3a02a3dd51
Lucas Pina <ti.lucaspina@gmail.com>
2022-06-28 21:03:42 -0300

    controller and category repository

--- src/app/routes.js
+++ src/app/routes.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 
 const Contactcontroller = require('./controllers/ContactController');
+const Categorycontroller = require('./controllers/CategoryController');
 
 const router = Router();
 
@@ -9,5 +10,8 @@ router.get('/contacts/:id', Contactcontroller.show);
 router.delete('/contacts/:id', Contactcontroller.delete);
 router.post('/contacts', Contactcontroller.store);
 router.put('/contact/:id', Contactcontroller.update);
+
+router.get('/categories', Categorycontroller.index);
+router.post('/categories', Categorycontroller.store);
 
 module.exports = router;
